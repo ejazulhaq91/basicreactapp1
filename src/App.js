4df44249097e9c581d296395f96e49047f5ed3d1
ejazulhaq91 <ejazulhaq91@yahoo.com>
2020-06-06 20:19:14 +0500

    adcd

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      Hello Everyone From Ejaz Ul Haq
+      Hello, Everyone From Ejaz Ul Haq
       This is assignment # 2 of Freelancing and Remot Work Online Bootcamp
     </div>
   );
