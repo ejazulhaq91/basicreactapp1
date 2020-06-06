31715f432716aa30bef1b13dd119091223252893
ejazulhaq91 <ejazulhaq91@yahoo.com>
2020-06-06 21:52:53 +0500

    1234

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 function App() {
   return (
     <div>
-      Hello, Everyone From Ejaz Ul Haq
-      This is assignment # 2 of Freelancing and Remot Work Online Bootcamp
+      <h1>Hello, Everyone From Ejaz Ul Haq</h1>
+      <p>This is assignment # 2 of Freelancing and Remot Work Online Bootcamp</p>
     </div>
   );
 }
